56c0b15a1a4dd0df1c04b88cfa0f9aa59a6820a4
Sina Heidari <83687995+HeidariSina@users.noreply.github.com>
2021-10-21 07:48:27 +0330

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/database";
+
+var firebaseConfig = {
+  // apikey :
+  // authDomain:
+  // projrctID:
+  // storageBucket:
+  // messagingSenderId:
+  // appId:
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage().ref("images");
+const audioStorage = firebase.storage().ref("audios");
+const createTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+const serverTimestamp = firebase.database.ServerValue.TIMESTAMP;
+
+export {
+  db,
+  auth,
+  provider,
+  storage,
+  audioStorage,
+  createTimestamp,
+  serverTimestamp,
+};
